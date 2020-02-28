1031ec19a6dc8e520faca3fc6a9a272fa6c14e04
Sabine Schluep <sabine.schluep@hva.nl>
2020-02-28 11:07:40 +0100

    id for POSterminal generate from 10000000, increment 1

--- src/main/java/nl/hva/makeitwork/bankit/bankitapplication/model/account/POSterminal.java
+++ src/main/java/nl/hva/makeitwork/bankit/bankitapplication/model/account/POSterminal.java
@@ -1,5 +1,8 @@
 package nl.hva.makeitwork.bankit.bankitapplication.model.account;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -8,12 +11,22 @@ import javax.persistence.ManyToOne;
 @Entity
 public class POSterminal {
     @Id
-    @GeneratedValue
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "POS_sequence"),
+                    @Parameter(name = "initial_value", value = "10000000"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    ) //Een 8 cijferig ID voor de pinautomaat, mag pas na koppeling doorgegeven worden aan de automaat.
     int POSid;
     @ManyToOne
     BusinessAccount account;
 
-    public POSterminal() {}
+    public POSterminal() {
+    }
 
     public POSterminal(int POSid, BusinessAccount account) {
         this.POSid = POSid;
