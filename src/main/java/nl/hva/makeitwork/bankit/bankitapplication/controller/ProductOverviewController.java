4497f30c8733fe0f05ee93bf59923071fc476df0
Sabine Schluep <sabine.schluep@hva.nl>
2020-03-05 08:39:50 +0100

    get bankaccount from customer instead of seperate list

--- src/main/java/nl/hva/makeitwork/bankit/bankitapplication/controller/ProductOverviewController.java
+++ src/main/java/nl/hva/makeitwork/bankit/bankitapplication/controller/ProductOverviewController.java
@@ -1,33 +1,33 @@
 package nl.hva.makeitwork.bankit.bankitapplication.controller;
 
+import nl.hva.makeitwork.bankit.bankitapplication.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import java.util.Optional;
 
 @Controller
 @SessionAttributes("customer")
 public class ProductOverviewController {
+    @Autowired
+    private CustomerService customerService;
 
  public ProductOverviewController(){super();}
 
-
     @GetMapping("selected_bankaccount")
-    public String bankaccountOverviewHandler (){
-    return "under_construction";
+    public String bankaccountOverviewHandler (Model model){
+     return "under_construction";
     }
 
     @GetMapping("open_account")
-    public String openAccountHandler (){
+    public String openAccountHandler (Model model){
         return "under_construction";
     }
 
     @GetMapping("connect_account")
-    public String connectAccountHandler (){
+    public String connectAccountHandler (Model model){
         return "under_construction";
     }
 }
