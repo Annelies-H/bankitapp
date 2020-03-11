923e37abb59d99982626b1b822ffb46aaf6e67d4
Annelies van Hoesel <avanhoesel@gmail.com>
2020-03-11 13:55:26 +0100

    ruwe pagina voor openen rekening toegevoegd

--- src/main/java/nl/hva/makeitwork/bankit/bankitapplication/controller/BankAccountController.java
+++ src/main/java/nl/hva/makeitwork/bankit/bankitapplication/controller/BankAccountController.java
@@ -16,13 +16,28 @@ public class BankAccountController {
         return "under_construction";
     }
 
-    @GetMapping(value = "open_account")
+    @GetMapping(value = "new")
     public String openAccountHandler (Model model){
+        return "new_account";
+    }
+
+    @GetMapping(value = "new/private")
+    public String openPrivateAccountHandler (Model model){
+        return "under_construction";
+    }
+
+    @GetMapping(value = "new/business")
+    public String openBusinessAccountHandler (Model model){
         return "under_construction";
     }
 
     @GetMapping("connect_account")
     public String connectAccountHandler (Model model){
         return "under_construction";
+    }
+
+    @GetMapping("overview")
+    public String accountOverviewHandler (Model model) {
+        return "product_overview";
     }
 }
