d9a22ba8444d05cc4f25e9f7de999e36805c6e93
Sabine Schluep <sabine.schluep@hva.nl>
2020-03-21 12:10:05 +0100

    added request POS button

--- src/main/java/nl/hva/makeitwork/bankit/bankitapplication/controller/POSController.java
+++ src/main/java/nl/hva/makeitwork/bankit/bankitapplication/controller/POSController.java
@@ -1,11 +1,19 @@
 package nl.hva.makeitwork.bankit.bankitapplication.controller;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+@Controller
+@SessionAttributes("customer")
 @RequestMapping (value = "/POS")
 public class POSController {
+
+    @GetMapping("request_POS")
+    public String requestPOSHandler (Model model ){
+
+        return "under_construction";
+    }
+
 }
