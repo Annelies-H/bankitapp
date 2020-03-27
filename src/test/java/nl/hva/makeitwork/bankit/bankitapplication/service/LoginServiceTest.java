b6a3b9cbd0fd5b2b911ea3a14454a5d3f55e5f1f
Gerben Koster <gerben.koster@hva.nl>
2020-03-27 13:00:18 +0100

    maeven PoM libraries voor testen toegevoegd

---
+++ src/test/java/nl/hva/makeitwork/bankit/bankitapplication/service/LoginServiceTest.java
@@ -0,0 +1,17 @@
+package nl.hva.makeitwork.bankit.bankitapplication.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+public class LoginServiceTest {
+
+    @Autowired
+    private LoginService loginService;
+
+}
