fad0d3bd7efaccb29e543a59b72d084f34bc2faf
Gerben Koster <gerben.koster@hva.nl>
2020-04-09 15:34:35 +0200

    foei annelies

--- testdata/TestCustomersController.java
+++ testdata/TestCustomersController.java
@@ -4,7 +4,6 @@ package nl.hva.makeitwork.bankit.bankitapplication.controller;
 
 
 
-import nl.hva.makeitwork.bankit.bankitapplication.service.GenerateCustomersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
